ad85d42fe23b97c2a5ee504c0907ea1383197ce1
NagibinA <91381276+NagibinA@users.noreply.github.com>
2026-08-15 13:49:57 +0300

    Update config_flow.py

--- custom_components/novosti_mchs/config_flow.py
+++ custom_components/novosti_mchs/config_flow.py
@@ -1,13 +1,19 @@
+"""Настройка интеграции Новости МЧС."""
 import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.core import callback
 from homeassistant.const import CONF_NAME
-from .const import DOMAIN, DEFAULT_NAME, CONF_RSS_URL, CONF_SOURCES_COUNT
+
+from .const import DOMAIN, DEFAULT_NAME, CONF_RSS_URL, CONF_SCAN_INTERVAL, DEFAULT_SCAN_INTERVAL
+
 
 class NovostiMCHSConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Конфигурационный поток для интеграции Новости МЧС."""
+
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
+        """Первый шаг настройки."""
         errors = {}
         
         if user_input is not None:
@@ -27,8 +33,8 @@ class NovostiMCHSConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         data_schema = vol.Schema({
             vol.Required(CONF_NAME, default=DEFAULT_NAME): str,
             vol.Required(CONF_RSS_URL, default="https://78.mchs.gov.ru/deyatelnost/press-centr/operativnaya-informaciya/rss"): str,
-            vol.Optional(CONF_SOURCES_COUNT, default=1): vol.All(
-                vol.Coerce(int), vol.Range(min=1, max=5, msg="Можно от 1 до 5 сенсоров")
+            vol.Optional(CONF_SCAN_INTERVAL, default=DEFAULT_SCAN_INTERVAL): vol.All(
+                vol.Coerce(int), vol.Range(min=60, max=3600, msg="Интервал от 60 до 3600 секунд")
             ),
         })
 
@@ -44,14 +50,18 @@ class NovostiMCHSConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     @staticmethod
     @callback
     def async_get_options_flow(config_entry):
+        """Возвращает поток опций."""
         return NovostiMCHSOptionsFlow(config_entry)
 
 
 class NovostiMCHSOptionsFlow(config_entries.OptionsFlow):
+    """Поток опций для интеграции Новости МЧС."""
+
     def __init__(self, config_entry):
         self.config_entry = config_entry
 
     async def async_step_init(self, user_input=None):
+        """Шаг настройки опций."""
         if user_input is not None:
             return self.async_create_entry(title="", data=user_input)
 
@@ -59,8 +69,8 @@ class NovostiMCHSOptionsFlow(config_entries.OptionsFlow):
             step_id="init",
             data_schema=vol.Schema({
                 vol.Required(CONF_RSS_URL, default=self.config_entry.data.get(CONF_RSS_URL)): str,
-                vol.Optional(CONF_SOURCES_COUNT, default=self.config_entry.data.get(CONF_SOURCES_COUNT, 1)): vol.All(
-                    vol.Coerce(int), vol.Range(min=1, max=5)
+                vol.Optional(CONF_SCAN_INTERVAL, default=self.config_entry.data.get(CONF_SCAN_INTERVAL, DEFAULT_SCAN_INTERVAL)): vol.All(
+                    vol.Coerce(int), vol.Range(min=60, max=3600)
                 ),
             })
         )
