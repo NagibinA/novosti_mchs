769f3ea7a8cffb2d32a24385b28cc0d0ada49e6a
NagibinA <91381276+NagibinA@users.noreply.github.com>
2026-08-15 10:46:04 +0300

    Update const.py

--- custom_components/novosti_mchs/const.py
+++ custom_components/novosti_mchs/const.py
@@ -4,4 +4,5 @@ DEFAULT_SCAN_INTERVAL = 600  # 10 минут
 MAX_ARTICLES = 10
 CONF_RSS_URL = "rss_url"
 CONF_SOURCES_COUNT = "sources_count"
+CONF_SCAN_INTERVAL = "scan_interval"  # ← ДОБАВЬТЕ ЭТУ СТРОКУ
 ATTR_ARTICLES = "articles"
