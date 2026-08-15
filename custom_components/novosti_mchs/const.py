f8dad139e5ee867d924f7f8a815238533711e5c5
NagibinA <91381276+NagibinA@users.noreply.github.com>
2026-08-15 09:49:49 +0300

    Create const.py

---
+++ custom_components/novosti_mchs/const.py
@@ -0,0 +1,7 @@
+DOMAIN = "novosti_mchs"
+DEFAULT_NAME = "Новости МЧС"
+DEFAULT_SCAN_INTERVAL = 600  # 10 минут
+MAX_ARTICLES = 10
+CONF_RSS_URL = "rss_url"
+CONF_SOURCES_COUNT = "sources_count"
+ATTR_ARTICLES = "articles"
